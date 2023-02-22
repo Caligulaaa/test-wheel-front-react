404645be50110c1a862c334b3be5a911b7313c29
caligula <none@none.com>
2023-02-22 10:43:35 +0200

    decomposition,fix spin after upload(fix first spin undefined) and fix other spin with Promise

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 // import './App.css';
 
 import RowRandom from './page/RowRand';
+import { useFetching } from './page/useFetching';
 
 function App() {
+  const [allItems,setallItems] = useState([])
+  const [fetchPost,isPostsLoading] = useFetching(() => new Promise((resolve,reject) => {
+    //get request all items for speen 
+    const getItems = [{id:0,name:'name12345'},{id:1,name:'name1111'},
+                    {id:2,name:'name2222'},{id:3,name:'name3333'},
+                    {id:4,name:'name4444'},{id:5,name:'name5555'},
+                    {id:6,name:'name6666'},{id:7,name:'name7777'},
+                    {id:8,name:'name8888'},{id:9,name:'name9999'},
+                    {id:10,name:'name0000'},{id:11,name:'name54321'},]
+    setTimeout(() => {
+      setallItems(getItems)
+      resolve()
+    }, 0)
 
+    // setallItems(getItems)
+    // setallItems(getItems)
+    //вот єту херню хз как закинуть что снизу если еще setTimeout будет как будто с запроса
+    
+}));
+
+useEffect(() => {
+  fetchPost()
+}, [])
 
 
   return (
     <div className="App">
-      <RowRandom />
+      {isPostsLoading
+      ?<h1>loading...</h1>
+      :<RowRandom myItems ={allItems}/>
+      }
+      
     </div>
   );
 }
--- src/page/RowRand.jsx
+++ src/page/RowRand.jsx
@@ -1,50 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import './CSSReact/style.css'
-import logo from './IMG/pointer.png'
 import { useFetching } from './useFetching';
 
+const RowRandom = (props) =>{
+    const cells = 91
 
-const RowRandom = () =>{
-    const cells = 41
-
-    const [allItems,setallItems] = useState([])
-    const [spinResult, setSpinResult] = useState([]);
-    //хук который если запрос пришел isPostsLoading True если еще не пришел false
-    // для проверки в html
-    const [fetchPost,isPostsLoading,posrError] = useFetching(async() => {
-            //get request all items for speen 
-            const getItems = [{id:0,name:'name12345'},{id:1,name:'name1111'},
-                            {id:2,name:'name2222'},{id:3,name:'name3333'},
-                            {id:4,name:'name4444'},{id:5,name:'name5555'},
-                            {id:6,name:'name6666'},{id:7,name:'name7777'},
-                            {id:8,name:'name8888'},{id:9,name:'name9999'},
-                            {id:10,name:'name0000'},{id:11,name:'name54321'},]
-            // setTimeout(() => {
-            //     setallItems(allitems)
-            // }, 1000)
-            
-            setallItems(getItems)
-            //вот єту херню хз как закинуть что снизу если еще setTimeout будет как будто с запроса
-            // generateItems()
-      }) 
-
+    const [allItems] = useState(props.myItems)
     
+    const [spinResult, setSpinResult] = useState([]);
+   
+    const [fetchPost,isPostsLoading] = useFetching(() => new Promise((resolve,reject) =>{
+        
+        const item = allItems[Math.floor(Math.random()*allItems.length)]
+        // get item from beckend
+        console.log(item)
+        setTimeout(() => {
+            //тут какаято фигня но только через фаинд оно перерисовыет и выдает то что тянет с бекенда
+            // оно имя ток перерисовывает а айди оставляет старый но в бекенд пишет то что и должно а рисует нужное имя
+            // плюс первый спин тоже работает с дополнительной проверкой 45 строка
+            const found = allItems.find(a => a.name = item.name);
+            // console.log(found)
+            setSpinResult(found)
+           
 
-    // тут получаю с бекенда нужный елемент рандом на беке написан 
-    const getItemFromBeckend = () => {
-        console.log(allItems)
-        // условно выпал 
-        const item =  {id:8,name:'name8888'}
-        setSpinResult(item)
-    }
+            start()
+            resolve()
+          }, 1000)
 
 
-
-    function getItem() {
-        var item = allItems[Math.floor(Math.random()*allItems.length)]
-        return item
-
-        }
+    }));
         
     function generateItems() {
         document.querySelector('.list').remove()
@@ -53,8 +37,11 @@ const RowRandom = () =>{
         `
         const list = document.querySelector('.list')
         for (let i = 0; i < cells; i++) {
-            let item = getItem()
-            if (i==20) item = spinResult
+            let item = allItems[Math.floor(Math.random()*allItems.length)]
+            if (i===45) item = spinResult
+
+            //without undefine after first spin
+            if (spinResult.length === 0 && i === 45) item = allItems[0]
             const li = document.createElement('li')
             li.setAttribute('data-item', JSON.stringify(item))
             li.classList.add('list__item')
@@ -66,40 +53,27 @@ const RowRandom = () =>{
     }
 
     useEffect(() => {
-        // generateItems()
-        fetchPost()
-        
-    }, [])
+        if(allItems?.length > 0) {
+            generateItems();
+        }
+    }, [allItems])
 
-    
 
-    let isStarted = false
-    let isFirstStart = true
-
-    //анимация спинера нужно както запустить вместе с кнопкой что делает запрос на 
-    //getItemFromBeckend
     function start() {
 
-        if (isStarted) return
-        else isStarted = true
-
-        if (!isFirstStart) generateItems()
-        else isFirstStart = false
+        generateItems()
 
         const list = document.querySelector('.list')
-        // list.setAttribute('data-item', JSON.stringify(spinResult))
         setTimeout(() => {
             list.style.left = '50%'
             list.style.transform = 'translate3d(-50%, 0, 0)'
         }, 0)
         
-        // console.log(list.querySelectorAll('li')[45].setAttribute('data-item', JSON.stringify(spinResult)))
-        const item = list.querySelectorAll('li')[20]
+        const item = list.querySelectorAll('li')[45]
         list.addEventListener('transitionend', () => {
-            isStarted = false
             item.classList.add('active')
             const data = JSON.parse(item.getAttribute('data-item'))
-            
+            // this get item from view
             console.log(data);
         }, {once: true})
     }
@@ -108,16 +82,15 @@ const RowRandom = () =>{
     return (
             <div className="myBody">
                 <div className="apple">
-                    <img className="pointer" src={logo} alt="" />
+                    <img className="pointer"  alt="" />
                     <div className="scope">
                         <ul className="list" />
                     </div>
-                    
                     {isPostsLoading
-                    ?<h1>не загрузило еще</h1>
-                    :<button onClick={getItemFromBeckend} className="start">Spin</button>
+                        ?<button onClick={fetchPost} className="start" disabled>Spin</button>
+                        :<button onClick={fetchPost} className="start">Spin</button>
                     }
-                    {/* <button onClick={getItemFromBeckend} className="start">Spin</button> */}
+                    
                 </div>
             </div>
             
